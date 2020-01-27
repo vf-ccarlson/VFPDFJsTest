4f034056fd76b6a242ea07ac55f693e973a867c3
vf-mbrady <mbrady@verifiedfirst.com>
2020-01-27 15:45:17 -0700

    getting closer to figuring out the file thing

--- src/app/pdf-parser/pdf-parser.component.ts
+++ src/app/pdf-parser/pdf-parser.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+// import { miniPdf } from '../../pdfParse/minipdf.js';
+import * as miniPdf from '../../pdfParse/minipdf.js'
 declare var minipdf: any;
+
 
 
 @Component({
@@ -16,16 +20,21 @@ export class PdfParserComponent implements OnInit {
 
   fileUploaded = false;
   parsedPDF = {};
+  file: File;
+  fileBlob: Blob;
 
 
-  initiateParse(e){
-    var file = e.target.files[0];
+  initiateParse(e: NgForm) {
+    console.log('hittin da initiate');
+    console.log(e);
+    var file = this.file;
     var reader = new FileReader();
     var _this = this
-		reader.onload = function() {
-			_this.on_file(file.name, reader.result);
+    reader.onload = function () {
+      _this.on_file(file.name, reader.result);
     };
-		reader.readAsArrayBuffer(file);
+    reader.readAsArrayBuffer(file);
+    console.log(file);
   }
 
   current_buffer = 'string'
@@ -38,11 +47,32 @@ export class PdfParserComponent implements OnInit {
 
   list(buf) {
     try {
-      this.parsedPDF = minipdf.list_fields(buf);
+      this.parsedPDF = miniPdf.list_fields(buf);
       this.fileUploaded = true;
     } catch (e) {
       // on_error(e);
       return;
     }
+  }
+
+  changeFile(file) {
+    return new Promise((resolve, reject) => {
+      const reader = new FileReader();
+      reader.readAsDataURL(file);
+      reader.onload = () => resolve(reader.result);
+      reader.onerror = error => reject(error);
+    });
+  }
+
+  uploadFile(event) {
+    if (event.target.value) {
+      const file = event.target.files[0];
+      const type = file.type;
+      this.changeFile(file).then((base64: string): any => {
+        console.log(base64);
+        this.fileBlob = this.b64Blob([base64], type);
+        console.log(this.fileBlob)
+      });
+    } else alert('Nothing')
   }
 }
